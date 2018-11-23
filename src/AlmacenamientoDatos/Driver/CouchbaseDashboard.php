e365d69f3f305dacd4ea99b6dab0c4e328410629
Rigoberto Reyes <rigoberto.reyes@gmail.com>
2018-11-22 22:36:48 -0600

    Verificaciones iniciales para obtener datos de coucbase

--- src/AlmacenamientoDatos/Driver/CouchbaseDashboard.php
+++ src/AlmacenamientoDatos/Driver/CouchbaseDashboard.php
@@ -11,10 +11,11 @@ class CouchbaseDashboard implements DashboardInterface
 {
     private $em;
     private $doc = 'origen_';
+    private $docIndicador = 'indicador_';
     private $bucketName = 'etab_origenes';
     private $bucket;
-    private $bucketNameIndicadores = 'etab_indicadores';
-    private $bucketIndicadores ;
+    private $bucketNameIndicador = 'etab_indicadores';
+    private $bucketIndicador ;
 
     public function __construct(EntityManager $em, ParameterBagInterface $params)
     {
@@ -29,14 +30,32 @@ class CouchbaseDashboard implements DashboardInterface
         // Authenticate, then open bucket
         $cluster->authenticate($authenticator);
         $this->bucket = $cluster->openBucket($this->bucketName);
-        $this->bucketIndicadores = $cluster->openBucket($this->bucketNameIndicadores);
+        $this->bucketIndicador = $cluster->openBucket($this->bucketNameIndicador);
 
     }
 
 
     public function crearIndicador(FichaTecnica $fichaTec, $dimension, $filtros) {
 
-        $this->fichaRepository->crearIndicador($fichaTec, $dimension, $filtros);
+        //Verificar si existe el documento de datos del indicador
+        $existe = $this->existeDocumento($this->bucketIndicador, $this->docIndicador.$fichaTec->getId());
+
+        // Verificar si la última vez que se cargaron datos para orígenes del indicador es posterior a la última
+        // vez que se mostró en el tablero
+        if ($fichaTec->getUpdatedAt() != '' and $fichaTec->getUltimaLectura() != '' and $existe == true) {
+            if ($fichaTec->getUltimaLectura() < $fichaTec->getUpdatedAt()){
+                //Retornar, no es necesario actualizar el documento del indicador
+                return true;
+            }
+        }
+
+        //Los campos de la ficha técnica determinan que campos se utilizarán de los orígenes de datos
+        $campos = str_replace(' ', '', $fichaTec->getCamposIndicador());
+
+        //Recuperar los datos de los orígenes asociados a cada variable del indicador
+        foreach ($fichaTec->getVariables() as $variable) {
+            $origenesIds[] = $variable->getOrigenDatos()->getId();
+        }
     }
 
     public function calcularIndicador($fichaTec, $dimension, $filtros, $verSql){
@@ -54,5 +73,16 @@ class CouchbaseDashboard implements DashboardInterface
 
     public function crearCamposIndicador (FichaTecnica $fichaTec) {
         $this->fichaRepository->crearCamposIndicador($fichaTec);
+    }
+
+    private function existeDocumento($bucket, $docName){
+
+        $existe = true;
+        try {
+            $bucket->get( $docName );
+        } catch (\Exception $e){
+            $existe = false;
+        }
+        return $existe;
     }
 }
